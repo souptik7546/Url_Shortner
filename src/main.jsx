51c97b014f7089947d3be5809334372717c86bca
Souptik Biswas <souptikbiswas@Souptiks-MacBook-Air.local>
2025-07-02 01:09:19 +0530

    Resolve app.jsx errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Layout } from './Layout.jsx'
 import { Dashboard } from './pages/Dashboard.jsx'
